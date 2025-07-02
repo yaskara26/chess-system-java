6cea58cb2a348b15ed9c437f24ad07a3214c3ab2
Yaskara Santangelo <yaskaraguimaraes@hotmail.com>
2025-07-02 14:18:36 -0300

    Switching player each turn

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,34 +13,32 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		
-		while(true) {
+
+		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces());
+				UI.printMatch(chessMatch);
 				System.out.println();
 				System.out.print("source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-				
+
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
-				
+
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-				
+
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-			}
-			catch (ChessException e) {
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
-			catch (InputMismatchException e) {
-				System.out.println(e.getMessage());
-				sc.nextLine();
-			}
-			
+
 		}
 	}
 
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -3,7 +3,7 @@ package boardgame;
 public abstract class Piece {
 	protected Position position;
 	private Board board;
-	
+
 	public Piece(Board board) {
 		this.board = board;
 //		não precisaria por q a position é null por padrao o java já interpretaria assim por não botarmos nada
@@ -13,25 +13,24 @@ public abstract class Piece {
 	protected Board getBoard() {
 		return board;
 	}
-	
-	
+
 	public abstract boolean[][] possibleMoves();
-	
+
 	public boolean possibleMove(Position position) {
 		return possibleMoves()[position.getRow()][position.getColumn()];
 	}
-	
+
 	public boolean isThereAnyPossibleMoves() {
 		boolean[][] mat = possibleMoves();
-		
-		for (int i=0; i<mat.length; i++) {
-			for (int j = 0; j<mat.length; j++) {
-				if(mat[i][j]) {
+
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if (mat[i][j]) {
 					return true;
 				}
 			}
 		}
 		return false;
 	}
-	
+
 }
