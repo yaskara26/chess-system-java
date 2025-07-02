176c8dfc781d0780dfc5e4182ae43e212b913c9a
Yaskara Santangelo <yaskaraguimaraes@hotmail.com>
2025-07-02 18:44:16 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -101,9 +101,10 @@ public class ChessMatch {
 			throw new ChessException("The chosen piece can't move to target postion");
 		}
 	}
-
+//realizar o movimento
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		if (capturedPiece != null) {
@@ -112,9 +113,10 @@ public class ChessMatch {
 		}
 		return capturedPiece;
 	}
-
+// desfazer o movimento
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece)board.removePiece(target);
+		p.decreaseMoveCount();
 		board.placePiece(p, source);
 
 		if (capturedPiece != null) {
@@ -189,26 +191,29 @@ public class ChessMatch {
 	}
 
 	private void initialSetup() {
+
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 8, new King(board, Color.BLACK));
+
+		// para testar o CHECKMATE
 		/*
-		 * placeNewPiece('c', 1, new Rook(board, Color.WHITE)); placeNewPiece('c', 2,
-		 * new Rook(board, Color.WHITE)); placeNewPiece('d', 2, new Rook(board,
-		 * Color.WHITE)); placeNewPiece('e', 2, new Rook(board, Color.WHITE));
-		 * placeNewPiece('e', 1, new Rook(board, Color.WHITE)); placeNewPiece('d', 1,
-		 * new King(board, Color.WHITE));
+		 * placeNewPiece('h', 7, new Rook(board, Color.WHITE)); placeNewPiece('d', 1,
+		 * new Rook(board, Color.WHITE)); placeNewPiece('e', 1, new King(board,
+		 * Color.WHITE));
 		 * 
-		 * placeNewPiece('c', 7, new Rook(board, Color.BLACK)); placeNewPiece('c', 8,
-		 * new Rook(board, Color.BLACK)); placeNewPiece('d', 7, new Rook(board,
-		 * Color.BLACK)); placeNewPiece('e', 7, new Rook(board, Color.BLACK));
-		 * placeNewPiece('e', 8, new Rook(board, Color.BLACK)); placeNewPiece('d', 8,
+		 * placeNewPiece('b', 8, new Rook(board, Color.BLACK)); placeNewPiece('a', 8,
 		 * new King(board, Color.BLACK));
 		 */
-
-		placeNewPiece('h', 7, new Rook(board, Color.WHITE));
-		placeNewPiece('d', 1, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 1, new King(board, Color.WHITE));
-
-		placeNewPiece('b', 8, new Rook(board, Color.BLACK));
-		placeNewPiece('a', 8, new King(board, Color.BLACK));
-
 	}
 }
